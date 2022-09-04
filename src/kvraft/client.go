1a582cf149a117f956d4d940db5654b44c0d9e40
Jack Zhang <jack333zhang@gmail.com>
2022-09-04 23:07:44 +0800

    Lab 3A: TestBasic3A

--- src/kvraft/client.go
+++ src/kvraft/client.go
@@ -1,30 +1,29 @@
 package kvraft
 
-import "6.824/labrpc"
-import "crypto/rand"
-import "math/big"
-
+import (
+	"6.824/labrpc"
+	"fmt"
+	"log"
+	"math/rand"
+	"sync/atomic"
+	"time"
+)
 
 type Clerk struct {
 	servers []*labrpc.ClientEnd
 	// You will have to modify this struct.
-}
-
-func nrand() int64 {
-	max := big.NewInt(int64(1) << 62)
-	bigx, _ := rand.Int(rand.Reader, max)
-	x := bigx.Int64()
-	return x
+	id  string
+	seq atomic.Int64
 }
 
 func MakeClerk(servers []*labrpc.ClientEnd) *Clerk {
 	ck := new(Clerk)
 	ck.servers = servers
 	// You'll have to add code here.
+	ck.id = randomString(10)
 	return ck
 }
 
-//
 // fetch the current value for a key.
 // returns "" if the key does not exist.
 // keeps trying forever in the face of all other errors.
@@ -35,14 +34,34 @@ func MakeClerk(servers []*labrpc.ClientEnd) *Clerk {
 // the types of args and reply (including whether they are pointers)
 // must match the declared types of the RPC handler function's
 // arguments. and reply must be passed as a pointer.
-//
 func (ck *Clerk) Get(key string) string {
-
+	seq := ck.nextSeq()
 	// You will have to modify this function.
+	for i := range ck.servers {
+		args := GetArgs{
+			Key:      key,
+			ClerkId:  ck.id,
+			ClerkSeq: seq,
+		}
+		reply := GetReply{}
+		ck.servers[i].Call("KVServer.Get", &args, &reply)
+		switch reply.Err {
+		case OK:
+			return reply.Value
+		case ErrNoKey:
+			return ""
+		case ErrWrongLeader:
+		default:
+			log.Fatalf("Clerk-%v Get args:%v reply: %v", ck.id, args, reply)
+		}
+	}
 	return ""
 }
 
-//
+func (ck *Clerk) nextSeq() int64 {
+	return ck.seq.Add(1)
+}
+
 // shared by Put and Append.
 //
 // you can send an RPC with code like this:
@@ -51,14 +70,44 @@ func (ck *Clerk) Get(key string) string {
 // the types of args and reply (including whether they are pointers)
 // must match the declared types of the RPC handler function's
 // arguments. and reply must be passed as a pointer.
-//
 func (ck *Clerk) PutAppend(key string, value string, op string) {
 	// You will have to modify this function.
+	seq := ck.nextSeq()
+	for {
+		for i := range ck.servers {
+			args := PutAppendArgs{
+				Key:      key,
+				Value:    value,
+				Op:       op,
+				ClerkId:  ck.id,
+				ClerkSeq: seq,
+			}
+			reply := PutAppendReply{}
+			ck.servers[i].Call("KVServer.PutAppend", &args, &reply)
+			switch reply.Err {
+			case OK:
+				return
+			case ErrWrongLeader:
+			case ErrNoKey:
+				log.Fatalf("Clerk-%v PutAppend args:%v reply: %v", ck.id, args, reply)
+			default:
+				log.Fatalf("Clerk-%v PutAppend args:%v reply: %v", ck.id, args, reply)
+			}
+		}
+		time.Sleep(100 * time.Millisecond)
+	}
 }
 
 func (ck *Clerk) Put(key string, value string) {
-	ck.PutAppend(key, value, "Put")
+	ck.PutAppend(key, value, PUT)
 }
 func (ck *Clerk) Append(key string, value string) {
-	ck.PutAppend(key, value, "Append")
+	ck.PutAppend(key, value, APPEND)
+}
+
+func randomString(length int) string {
+	rand.Seed(time.Now().UnixNano())
+	b := make([]byte, length)
+	rand.Read(b)
+	return fmt.Sprintf("%x", b)[:length]
 }
--- src/kvraft/server.go
+++ src/kvraft/server.go
@@ -7,9 +7,10 @@ import (
 	"log"
 	"sync"
 	"sync/atomic"
+	"time"
 )
 
-const Debug = false
+const Debug = true
 
 func DPrintf(format string, a ...interface{}) (n int, err error) {
 	if Debug {
@@ -18,11 +19,17 @@ func DPrintf(format string, a ...interface{}) (n int, err error) {
 	return
 }
 
+const CommitTimeOut = 500 * time.Millisecond
 
 type Op struct {
 	// Your definitions here.
 	// Field names must start with capital letters,
 	// otherwise RPC will break.
+	Key      string
+	Value    string
+	OpType   string
+	ClerkId  string
+	ClerkSeq int64
 }
 
 type KVServer struct {
@@ -35,18 +42,75 @@ type KVServer struct {
 	maxraftstate int // snapshot if log grows this big
 
 	// Your definitions here.
+	state       map[string]string           // kv table
+	ClerkResult map[string]map[int64]string // Clerk RPC result
 }
-
 
 func (kv *KVServer) Get(args *GetArgs, reply *GetReply) {
 	// Your code here.
+	op := Op{
+		Key:      args.Key,
+		OpType:   GET,
+		ClerkId:  args.ClerkId,
+		ClerkSeq: args.ClerkSeq,
+	}
+	_, _, isLeader := kv.rf.Start(op)
+	if !isLeader {
+		reply.Err = ErrWrongLeader
+		return
+	}
+
+	before := time.Now()
+	for time.Now().Sub(before) < CommitTimeOut {
+		kv.mu.Lock()
+		value, ok := kv.getClerkReq(op.ClerkId, op.ClerkSeq)
+		if ok {
+			reply.Err = OK
+			reply.Value = value
+			kv.mu.Unlock()
+			return
+		}
+
+		kv.mu.Unlock()
+		time.Sleep(30 * time.Millisecond)
+	}
+	reply.Err = ErrWrongLeader
 }
 
 func (kv *KVServer) PutAppend(args *PutAppendArgs, reply *PutAppendReply) {
 	// Your code here.
+	if !(args.Op == PUT || args.Op == APPEND) {
+		log.Fatalf("failure: KV PutAppend wrong args: %v", args)
+	}
+	op := Op{
+		Key:      args.Key,
+		Value:    args.Value,
+		OpType:   args.Op,
+		ClerkId:  args.ClerkId,
+		ClerkSeq: args.ClerkSeq,
+	}
+	_, _, isLeader := kv.rf.Start(op)
+	if !isLeader {
+		reply.Err = ErrWrongLeader
+		return
+	}
+
+	before := time.Now()
+	for time.Now().Sub(before) < CommitTimeOut {
+		kv.mu.Lock()
+		_, ok := kv.getClerkReq(op.ClerkId, op.ClerkSeq)
+		if ok {
+			reply.Err = OK
+			kv.mu.Unlock()
+			return
+		}
+
+		kv.mu.Unlock()
+		time.Sleep(30 * time.Millisecond)
+	}
+	reply.Err = ErrWrongLeader
 }
 
-//
 // the tester calls Kill() when a KVServer instance won't
 // be needed again. for your convenience, we supply
 // code to set rf.dead (without needing a lock),
@@ -55,7 +119,6 @@ func (kv *KVServer) PutAppend(args *PutAppendArgs, reply *PutAppendReply) {
 // code to Kill(). you're not required to do anything
 // about this, but it may be convenient (for example)
 // to suppress debug output from a Kill()ed instance.
-//
 func (kv *KVServer) Kill() {
 	atomic.StoreInt32(&kv.dead, 1)
 	kv.rf.Kill()
@@ -67,7 +130,6 @@ func (kv *KVServer) killed() bool {
 	return z == 1
 }
 
-//
 // servers[] contains the ports of the set of
 // servers that will cooperate via Raft to
 // form the fault-tolerant key/value service.
@@ -80,7 +142,6 @@ func (kv *KVServer) killed() bool {
 // you don't need to snapshot.
 // StartKVServer() must return quickly, so it should start goroutines
 // for any long-running work.
-//
 func StartKVServer(servers []*labrpc.ClientEnd, me int, persister *raft.Persister, maxraftstate int) *KVServer {
 	// call labgob.Register on structures you want
 	// Go's RPC library to marshall/unmarshall.
@@ -91,11 +152,67 @@ func StartKVServer(servers []*labrpc.ClientEnd, me int, persister *raft.Persiste
 	kv.maxraftstate = maxraftstate
 
 	// You may need initialization code here.
+	kv.state = make(map[string]string)
+	kv.ClerkResult = make(map[string]map[int64]string)
 
 	kv.applyCh = make(chan raft.ApplyMsg)
 	kv.rf = raft.Make(servers, me, persister, kv.applyCh)
 
 	// You may need initialization code here.
+	go kv.ticker()
 
 	return kv
+}
+
+func (kv *KVServer) ticker() {
+	for applyMsg := range kv.applyCh {
+		if applyMsg.CommandValid == false {
+			// ignore other types of ApplyMsg
+		} else {
+			kv.mu.Lock()
+			op := applyMsg.Command.(Op)
+			DPrintf("KV%d before apply op: %v value: %v", kv.me, op, kv.get(op.Key))
+			switch op.OpType {
+			case PUT:
+				kv.put(op.Key, op.Value)
+			case APPEND:
+				kv.append(op.Key, op.Value)
+			}
+			kv.putClerkReq(op.ClerkId, op.ClerkSeq, kv.get(op.Key))
+			DPrintf("KV%d after apply op: %v value: %v", kv.me, op, kv.get(op.Key))
+			kv.mu.Unlock()
+		}
+	}
+}
+
+func (kv *KVServer) get(key string) string {
+	return kv.state[key]
+}
+
+func (kv *KVServer) put(key, value string) {
+	kv.state[key] = value
+}
+
+func (kv *KVServer) append(key, value string) {
+	DPrintf("KV%d apply key: %v value: %v", kv.me, key, value)
+	kv.put(key, kv.get(key)+value)
+}
+
+func (kv *KVServer) getClerkReq(clerkId string, seq int64) (string, bool) {
+	m, value := kv.ClerkResult[clerkId]
+	if !value {
+		return "", value
+	}
+	op, value := m[seq]
+	return op, value
+}
+
+// todo: clean ClerkReq
+func (kv *KVServer) putClerkReq(clerkId string, seq int64, value string) {
+	m, ok := kv.ClerkResult[clerkId]
+	if !ok {
+		kv.ClerkResult[clerkId] = make(map[int64]string)
+		m = kv.ClerkResult[clerkId]
+	}
+	m[seq] = value
 }
